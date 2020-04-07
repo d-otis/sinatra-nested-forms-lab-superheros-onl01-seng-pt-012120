5bf8efb182a895c2118d7d575bea605ed94b3d84
Dan Foley <dan@dan-foley.com>
2020-04-07 17:46:43 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,20 @@ class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
 
+    get '/' do
+      erb :index
+    end
+
+    get '/team' do
+
+      erb :team
+    end
+
+    post '/teams' do
+      @team = params[:team]
+      redirect to '/team'
+    end
+
+
 
 end
